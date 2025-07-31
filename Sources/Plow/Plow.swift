59e5857c3553a7a63ad049376bd87c1cb3fef539
idontwanttothink <barquean.09excavacion@icloud.com>
2025-07-30 19:35:21 -0500

    Basic types

--- Sources/Plow/Plow.swift
+++ Sources/Plow/Plow.swift
@@ -1,2 +1,39 @@
-public struct PlowText {
+indirect enum PlowRope {
+	struct ParentData {
+		var weight: Int
+		var scalarWeight: Int
+	}
+
+	case Leaf(String)
+	case Parent(PlowRope, ParentData, PlowRope?)
+
+	private func collect(into: inout String) {
+		switch self {
+		case .Leaf(let str):
+			into += str
+		case .Parent(let left, _, let right):
+			left.collect(into: &into)
+			if let right = right {
+				right.collect(into: &into)
+			}
+		}
+	}
+
+	func collect() -> String {
+		var out = ""
+		collect(into: &out)
+		return out
+	}
+
+	func index() -> Character {
+		Character("a")
+	}
+	func index() -> UnicodeScalar {
+		UnicodeScalar(0)
+	}
+
+	/// - Parameter at: index of an extended grapheme cluster
+	func split(at: Int) -> (PlowRope, PlowRope) {
+		(.Leaf("hi"), .Leaf("hi"))
+	}
 }
