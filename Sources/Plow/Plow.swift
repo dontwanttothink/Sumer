7adafd105035862379bf3fba300e076947471bc0
idontwanttothink <barquean.09excavacion@icloud.com>
2025-08-03 23:37:58 -0500

    Think more carefully

--- Sources/Plow/Plow.swift
+++ Sources/Plow/Plow.swift
@@ -56,6 +56,7 @@ public class PlowRopeNode {
 
 	/// Create a new leaf.
 	init(ownedBy owner: PlowRope, content: String) throws {
+		self.owner = owner
 		self.color = .Black
 
 		if content.count > Self.maxLeafLength {
@@ -65,7 +66,12 @@ public class PlowRopeNode {
 		self.content = content
 	}
 
-	/// assumes that self.right != nil
+	// TODO: should these methods be updated to keep weights
+	// accurate?
+	/// Applies a left rotation operation from Red-Black Trees on this node. A
+	/// crash occurs if `self.right == nil`.
+	///
+	/// See ``rightRotate()``.
 	func leftRotate() {
 		let y = self.right!
 
@@ -90,7 +96,10 @@ public class PlowRopeNode {
 		self.parent = y
 	}
 
-	/// assumes that self.left != nil
+	/// Applies a right rotation operation from Red-Black Trees on this node. A
+	/// crash occurs if `self.left == nil`.
+	///
+	/// See ``leftRotate()``.
 	func rightRotate() {
 		let x = self.left!
 
@@ -114,151 +123,39 @@ public class PlowRopeNode {
 		x.right = self
 		self.parent = x
 	}
-
-	// we can implement concatenations through a split
-	// and two joins.
 }
 
-public class PlowRope: BidirectionalCollection {
+public class PlowRope /* : BidirectionalCollection */ {
 	public var count: Int
 	var root: PlowRopeNode?
 
 	init(for str: String) throws {
-		root = try PlowRopeNode(ownedBy: self, content: str)
+		self.count = str.count
+		self.root = try PlowRopeNode(ownedBy: self, content: str)
 	}
 
-	private func rb_insert_fixup(new node: PlowRopeNode) {
-		var n = node
-		while let p = n.parent, p.color == .Red {
-			if let ppl = p.parent?.left,
-				p === ppl
-			{
-				let uncle = p.parent!.right
-				if uncle?.color ?? .Black == .Red {
-					p.color = .Black
-					uncle?.color = .Black
-					p.parent!.color = .Red
-					n = p.parent!  // ? probably incorrect
-				} else {
-					if let pr = n.parent?.right, n === pr {
-						n = n.parent!
-						n.leftRotate()
-					}
-					n.parent?.color = .Black
-					n.parent?.parent?.color = .Red
-					n.parent?.parent?.rightRotate()
-				}
-			} else {
-				let uncle = p.parent?.left
-				if uncle?.color ?? .Black == .Red {
-					p.color = .Black
-					uncle!.color = .Black
-					p.parent?.color = .Red
-					n = p.parent!  // ditto
-				} else {
-					if n === n.parent?.left {
-						n = p
-						n.rightRotate()
-					}
-					p.color = .Black
-					p.parent!.color = .Red
-					n.parent!.parent!.leftRotate()
-				}
-			}
-		}
-		self.root!.color = .Black
+	/// Performs manipulations on the tree to fix imbalances after an internode
+	/// insertion.
+	///
+	/// - Parameter new: A ``PlowRopeNode`` returned by ``newInternode(at:)``.
+	/// - Returns: A leaf, _A_, such that the path from the root to _A_ includes
+	/// every node whose ``PlowRopeNode/weight`` is newly inconsistent.
+	private func redBlackFixup(dueTo new: PlowRopeNode) {
 	}
 
-	private func insert(str: String, at idx: Int) throws {
-		let strCount = str.count
-
-		var current = root
-
-		var parentData: (PlowRopeNode, Int)? = nil
-		var cidx = idx
-		while let c = current, !c.isLeaf {
-			parentData = (c, cidx)
-			if cidx == 0 || cidx < c.weight! {
-				current = c.left
-			} else {
-				cidx -= c.weight!
-				current = c.right
-			}
-		}
-
-		if current == nil || current!.isEmpty {
-			// There's an empty leaf available for this position.
-			let new = try PlowRopeNode(ownedBy: self, content: str)
-
-			if let (parent, _) = parentData {
-				if parent.right === current {
-					// most likely I believe, if not always the case
-					// (because why would we have a node with weight 0??)
-					parent.right = new
-				} else {
-					parent.weight = str.count
-					parent.left = new
-				}
-			} else {
-				// No root exists, so we need to make one.
-				self.root = PlowRopeNode(ownedBy: self, color: .Black)
-
-				self.count = strCount
-				root!.weight = strCount
-				root!.left = new
-			}
-
-			self.count += strCount
-			return
-		}
-
-		// 'current' is not nil and non-empty; thus it is also a leaf with a
-		// parent
-		let content = current!.content!
-		let (parent, pidx) = parentData!
-
-		let insertionIndex = content.index(content.startIndex, offsetBy: pidx)
-		let (left, right) = content.inserted(
-			contentsOf: str,
-			at: insertionIndex,
-			withLimit: PlowRopeNode.maxLeafLength
-		)
-
-		if let right {
-			// We need to create a new internal node.
-			let new = PlowRopeNode(ownedBy: self, color: .Red)
-			new.parent = parent
-
-			new.weight = left.count  // = maxLeafLength
-			new.left = try PlowRopeNode(ownedBy: self, content: left)
-			new.right = try PlowRopeNode(ownedBy: self, content: right)
-
-			if pidx < parent.weight! {
-				parent.left = new
-			} else {
-				parent.right = new
-			}
-
-			// fix weights
-			var c = new
-			while let p = c.parent, p.left === c {
-				p.weight! += strCount
-				c = p
-			}
-			rb_insert_fixup(new: new)
-		} else {
-			var c = current!
-			c.content = left
-
-			// fix weights
-			while let p = c.parent, p.left === c {
-				p.weight! += strCount
-				c = p
-			}
-		}
-
-		self.count += strCount
+	/// Inserts a new internode (non-content) suitable for large text insertion
+	/// at `index`.
+	///
+	/// The leaf containing the character at `index` is replaced with an
+	/// internode; the leaf is moved to its left child. The resulting tree may
+	/// not be balanced. Weights remain correct.
+	///
+	/// - Returns: The inserted internode.
+	private func newInternode(at index: Int) throws -> PlowRopeNode {
 	}
+
+	// we can implement concatenations through a split
+	// and two joins.
 }
 
 // Bibliography:
