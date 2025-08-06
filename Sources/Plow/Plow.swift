b3d8d6479f076723d31878108d870fa76c0789d0
dontwanttothink <barquean.09excavacion@icloud.com>
2025-08-05 21:31:57 -0600

    Think even more carefully

--- Sources/Plow/Plow.swift
+++ Sources/Plow/Plow.swift
@@ -16,165 +16,213 @@ extension String {
 	}
 }
 
-public class PlowRopeNode {
-	enum InitializationError: Error {
-		case TooLong
+public final class PlowRopeNode {
+	public static let maxLeafCount = 1_000_000
+
+	enum Data {
+		case Parental(ParentalNode)
+		case Leaf(LeafNode)
 	}
 
-	public static let maxLeafLength = 1_000_000
+	var data: Data!
 
-	enum Color {
-		case Red, Black
+	/// Creates a new leaf.
+	init(ownedBy owner: PlowRope, content: String) {
+		data = .Leaf(LeafNode(content, ownedBy: owner, forContainer: self))
 	}
 
-	unowned let owner: PlowRope
-
-	var color: Color
-
-	var left: PlowRopeNode?
-	var right: PlowRopeNode?
-	var parent: PlowRopeNode?
-
-	var weight: Int?
-	// possible: scalarWeight
-	var content: String?
-
-	var isLeaf: Bool {
-		return content != nil
-	}
-	var isEmpty: Bool {
-		return content != nil && content!.isEmpty
+	/// Creates a new parental node.
+	init(
+		ownedBy owner: PlowRope, leftChild: PlowRopeNode? = nil,
+		rightChild: PlowRopeNode? = nil
+	) {
+		data = .Parental(
+			ParentalNode(
+				ownedBy: owner,
+				forContainer: self,
+				leftChild: leftChild ?? PlowRopeNode(ownedBy: owner, content: ""),
+				rightChild: rightChild ?? PlowRopeNode(ownedBy: owner, content: ""),
+			)
+		)
 	}
 
-	/// Create a new internode.
-	init(ownedBy owner: PlowRope, color: Color) {
-		self.owner = owner
-		self.color = color
-
-		self.weight = 0
-	}
-
-	/// Create a new leaf.
-	init(ownedBy owner: PlowRope, content: String) throws {
-		self.owner = owner
-		self.color = .Black
-
-		if content.count > Self.maxLeafLength {
-			throw InitializationError.TooLong
+	// Maybe: make this a macro
+	var count: Int {
+		switch data! {
+		case .Leaf(let content):
+			return content.count
+		case .Parental(let children):
+			return children.count
 		}
-
-		self.content = content
 	}
-
-	// TODO: These methods should be updated to keep weight numbers accurate.
-	//
-	/// Applies a left rotation operation from Red-Black Trees on this node. A
-	/// crash occurs if `self.right == nil`.
-	///
-	/// See ``rightRotate()``.
-	func leftRotate() {
-		let y = self.right!
-
-		self.right = y.left
-		if let yl = y.left {
-			yl.parent = self
+	var owner: PlowRope {
+		switch data! {
+		case .Leaf(let leaf):
+			return leaf.owner
+		case .Parental(let parent):
+			return parent.owner
 		}
-
-		y.parent = self.parent
-
-		if let parent = self.parent {
-			if self === parent.left {
-				parent.left = y
-			} else {
-				parent.right = y
+	}
+	var parent: ParentalNode? {
+		get {
+			switch data! {
+			case .Leaf(let node):
+				return node.parent
+			case .Parental(let node):
+				return node.parent
 			}
-		} else {
-			owner.root = y
 		}
-
-		y.left = self
-		self.parent = y
+		set {
+			switch data! {
+			case .Leaf(let node):
+				node.parent = newValue
+			case .Parental(let node):
+				node.parent = newValue
+			}
+		}
 	}
 
-	/// Applies a right rotation operation from Red-Black Trees on this node. A
-	/// crash occurs if `self.left == nil`.
-	///
-	/// See ``leftRotate()``.
-	func rightRotate() {
-		let x = self.left!
+	/// Obtain the parental node corresponding to this node. A crash occurs if
+	/// this method is called on a leaf.
+	func asParental() -> PlowRopeNode.ParentalNode {
+		guard case .Parental(let node) = self.data else {
+			fatalError("Attempted to use a leaf node as a parental node.")
+		}
+		return node
+	}
 
-		self.left = x.right
-		if let xr = x.right {
-			xr.parent = self
+	func isRightChildOf(_ node: PlowRopeNode) -> Bool {
+		if case .Parental(let data) = node.data,
+			data.right === self
+		{
+			return true
+		}
+		return false
+	}
+	func isRightChildOf(_ data: PlowRopeNode.ParentalNode) -> Bool {
+		if data.right === self {
+			return true
+		}
+		return false
+	}
+	func isLeftChildOf(_ node: PlowRopeNode) -> Bool {
+		if case .Parental(let data) = node.data,
+			data.left === self
+		{
+			return true
+		}
+		return false
+	}
+	func isLeftChildOf(_ data: PlowRopeNode.ParentalNode) -> Bool {
+		if data.left === self {
+			return true
+		}
+		return false
+	}
+
+	public final class ParentalNode {
+		unowned let owner: PlowRope
+		unowned let container: PlowRopeNode
+		weak var parent: ParentalNode?
+
+		var count: Int
+		var left: PlowRopeNode
+		var right: PlowRopeNode
+
+		var balanceFactor: Int
+
+		init(
+			ownedBy owner: PlowRope, forContainer container: PlowRopeNode,
+			leftChild: PlowRopeNode, rightChild: PlowRopeNode
+		) {
+			self.owner = owner
+			self.container = container
+
+			self.left = leftChild
+			self.right = rightChild
+			self.count = leftChild.count + rightChild.count
+			self.balanceFactor = 0
 		}
 
-		x.parent = self.parent
+		// TODO: keep counts correct
+		func rotateLeft(with z: ParentalNode) -> ParentalNode {
+			let inner = z.left
+			self.right = inner
 
-		if let parent = self.parent {
-			if self === parent.left {
-				parent.left = x
-			} else {
-				parent.right = x
+			if case .Parental(let node) = inner.data {
+				node.right.parent = self
 			}
-		} else {
-			owner.root = x
+			z.left = self.container
+			self.parent = z
+
+			if z.balanceFactor == 0 {
+				self.balanceFactor = 1
+				z.balanceFactor = -1
+			} else {
+				self.balanceFactor = 0
+				z.balanceFactor = 0
+			}
+			return z
 		}
 
-		x.right = self
-		self.parent = x
+		func rotateRight(with z: ParentalNode) -> ParentalNode {
+
+		}
+
+		func rotateRightLeft(with z: ParentalNode) -> ParentalNode {
+
+		}
+	}
+
+	public final class LeafNode {
+		unowned let owner: PlowRope
+		unowned let container: PlowRopeNode
+		weak var parent: ParentalNode?
+
+		var count: Int {
+			content.count
+		}
+		var content: String
+
+		init(
+			_ content: String, ownedBy owner: PlowRope,
+			forContainer container: PlowRopeNode
+		) {
+			self.owner = owner
+			self.container = container
+
+			self.content = content
+		}
 	}
 }
 
+// The root is never a leaf node.
 public class PlowRope /* : BidirectionalCollection */ {
-	public var count: Int
-	var root: PlowRopeNode?
+	public var count: Int {
+		root.count
+	}
+	var root: PlowRopeNode!
 
+	convenience init() {
+		try! self.init(for: "")
+	}
 	init(for str: String) throws {
-		self.count = str.count
-		self.root = try PlowRopeNode(ownedBy: self, content: str)
 	}
 
 	/// Performs manipulations on the tree to fix imbalances after an internode
-	/// insertion. Weights remain correct.
+	/// insertion. Sizes remain correct.
 	///
-	/// - Parameter new: A ``PlowRopeNode`` returned by ``newInternode(at:)``.
-	private func redBlackFixup(dueTo new: PlowRopeNode) {
+	/// - Parameter new: A ``PlowRopeNode/ParentalNode`` returned by
+	/// ``newInternode(at:)``.
+	private func insertionFixup(dueTo new: PlowRopeNode) {
 		var current = new
-		while let parent = current.parent, parent.color == .Red {
-			let grandparent = parent.parent
-			if let grandparent, parent === grandparent.left {
-				if let uncle = grandparent.right, uncle.color == .Red {
-					parent.color = .Black
-					uncle.color = .Black
-					grandparent.color = .Red
-					current = grandparent
-				} else {
-					if current === parent.right {
-						current = parent
-						current.leftRotate()
-					}
-					current.parent!.color = .Black
-					current.parent!.parent!.color = .Red
-					current.parent!.parent!.rightRotate()
-				}
-			} else {
-				// symmetrical, with 'left' and 'right' exchanged
-				if let grandparent,
-					let uncle = grandparent.left,
-					uncle.color == .Red
-				{
-					parent.color = .Black
-					uncle.color = .Black
-					grandparent.color = .Red
-					current = grandparent
-				} else {
-					if current === parent.left {
-						current = parent
-						current.rightRotate()
-					}
-					current.parent!.color = .Black
-					current.parent!.parent!.color = .Red
-					current.parent!.parent!.leftRotate()
+		var maybeParent = new.parent
+		while let parent = maybeParent {
+			var newRoot: PlowRopeNode
+			if current.isRightChildOf(parent) {
+				if parent.balanceFactor > 0 {
+					// right-heavy
+					let grandparent = parent.parent
 				}
 			}
 		}
@@ -185,10 +233,37 @@ public class PlowRope /* : BidirectionalCollection */ {
 	///
 	/// The leaf containing the character at `index` is replaced with an
 	/// internode; the leaf is moved to its left child. The resulting tree may
-	/// not be balanced. Weights remain correct.
+	/// not be balanced. Sizes remain correct.
 	///
 	/// - Returns: The inserted internode.
-	private func newInternode(at index: Int) throws -> PlowRopeNode {
+	private func insertInternode(at index: Int) throws -> PlowRopeNode {
+		precondition(index >= 0 && index <= self.count, "Index out of bounds")
+
+		var current = root!
+		var cidx = index
+		var parent: PlowRopeNode.ParentalNode!
+		var pidx: Int!
+		while case .Parental(let children) = current.data {
+			parent = children
+			pidx = cidx
+			if cidx <= current.count {
+				current = children.left
+			} else {
+				cidx -= current.count
+				current = children.right
+			}
+		}
+
+		let new = PlowRopeNode(ownedBy: self, leftChild: current)
+		new.parent = parent
+		current.parent = new.asParental()
+
+		if pidx <= parent.left.count {
+			parent.left = new
+		} else {
+			parent.right = new
+		}
+		return new
 	}
 
 	// we can implement concatenations through a split
@@ -196,5 +271,5 @@ public class PlowRope /* : BidirectionalCollection */ {
 }
 
 // Bibliography:
-// - Cormen, T. H., et al. Introduction to Algorithms, 4th ed., MIT Press, 2022. Red-Black Trees, Chapter 13
+// - AVL tree in Wikipedia. https://en.wikipedia.org/w/index.php?title=AVL_tree&oldid=1299115771
 // - Rope (data structure) in Wikipedia. https://en.wikipedia.org/w/index.php?title=Rope_(data_structure)&oldid=1290031069
