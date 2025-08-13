9246ea75d4634e641ebbe746530d7e34b0c06290
dontwanttothink <barquean.09excavacion@icloud.com>
2025-08-12 22:24:08 -0600

    Remove comment

--- Package.swift
+++ Package.swift
@@ -9,8 +9,6 @@ let package = Package(
 		.macOS(.v26)
 	],
 	targets: [
-		// Targets are the basic building blocks of a package, defining a module or a test suite.
-		// Targets can depend on other targets in this package and products from dependencies.
 		.target(name: "Plow", path: "Sources/Plow"),
 		.target(
 			name: "BridgedC",
