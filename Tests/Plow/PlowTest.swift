92b604af5be6d358d4801a239e1816771a010364
dontwanttothink <barquean.09excavacion@icloud.com>
2025-08-22 19:53:56 -0500

    Add more tests

--- Tests/Plow/PlowTest.swift
+++ Tests/Plow/PlowTest.swift
@@ -2,8 +2,22 @@ import Testing
 
 @testable import Plow
 
-@Test func keepsValue() throws {
-	let pr = PlowRope(for: "hello how are you?")
-	#expect(pr[0] == "h")
-	#expect(String(pr) == "hello how are you")
+@Test("Can be indexed correctly") func indexing() {
+	let pr = PlowRope(for: "Hello, how are you?")
+	#expect(pr[0] == "H")
+	#expect(pr[1] == "e")
+	#expect(pr[pr.count - 1] == "?")
+}
+
+@Test("Can be converted into a string") func convertToString() {
+	let pr = PlowRope(for: "I'm okay.")
+	#expect(String(pr) == "I'm okay.")
+}
+
+@Test("Can be joined together") func joinedTogether() {
+	let a = PlowRope(for: "I'm okay.")
+	let b = PlowRope(for: " But…")
+	let c = a.join(with: b)
+
+	#expect(String(c) == "I'm okay. But…")
 }
