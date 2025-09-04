c6921c27316d20576e6b556f47efeceda5b43e1d
dontwanttothink <barquean.09excavacion@icloud.com>
2025-09-04 17:30:08 -0500

    Show a PlowEditor

--- Sources/Sumer/Document/DiscreteFileView.swift
+++ Sources/Sumer/Document/DiscreteFileView.swift
@@ -1,15 +1,18 @@
+import CoreText
+import Plow
 import SwiftUI
 
 struct DiscreteFileView: View {
 	var paths: [URL]
 
 	var body: some View {
-		Text("Discrete files view !!")
+		Text("Discrete file view !!")
 		if !paths.isEmpty {
 			Text(
 				"You are editing "
 					+ paths.map({ $0.lastPathComponent }).joined(
 						separator: ", "))
 		}
+		PlowEditor()
 	}
 }
